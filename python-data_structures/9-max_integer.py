6f5f7fe4a2fa8f68272cf86873f32b6503db9006
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-09-10 10:49:12 +0200

    bug fixes of when list is empty and if list only has negative numbers

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == None:
+    if len(my_list) = 0:
         return None
-    biggest = 0
+    biggest = my_list[0]
     for i in my_list:
         if i > biggest:
             biggest = i
