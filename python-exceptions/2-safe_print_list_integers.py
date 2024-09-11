347e77db5567d10ffa91fd5b78975092fd65262a
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-09-11 13:26:36 +0200

    print all numbers now correctly

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,9 +3,9 @@ def safe_print_list_integers(my_list=[], x=0):
     printed = 0
     for i in range(0, x):
         try:
-            print("{:d}".format(my_list=[i]), end="")
+            print("{:d}".format(my_list[i]), end="")
             printed += 1
-        except (TypeError, ValueError):
+        except (ValueError, TypeError, IndexError):
             continue
     print("")
-    return (printed)
+    return printed
