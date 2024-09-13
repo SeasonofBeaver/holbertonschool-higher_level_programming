1df3de4272b2e6994e8dee0566ac1930f2fea7b0
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-09-13 10:46:22 +0200

    changed a bit of code

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -12,16 +12,16 @@ def text_indentation(text):
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
+
     char = 0
-    while char < len(text) and text[char] == " ":
+    while char < len(text) and text[char] == ' ':
         char += 1
     while char < len(text):
         print(text[char], end="")
         if text[char] == "\n" or text[char] in ".?:":
             if text[char] in ".?:":
-                print("")
-                print("")
-                char += 1
+                print("\n")
+            char += 1
             while char < len(text) and text[char] == ' ':
                 char += 1
             continue
