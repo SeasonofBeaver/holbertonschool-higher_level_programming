ead6e0741dd2da8585b46146b68ceaf03abde59d
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-09-16 10:52:49 +0200

    created an empty square class

---
+++ python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a Class for a Square"""
+
+
+class Square(self):
+    """
+    An empty class to define a square.
+    For now just a placeholder here for future use.
+    """
+    pass
