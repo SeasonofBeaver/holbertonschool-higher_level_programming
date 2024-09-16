5ce4d378b8f0eec3622567daa2bbe7852ca59bb5
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-09-16 11:12:16 +0200

    create a new square with the size size

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a Class for a Square"""
+
+
+class Square:
+    """
+    An empty class to define a square.
+    For now just a placeholder here for future use.
+    """
+    def __init__(self, size):
+        """Creates a new square.
+
+        Args:
+            size (int): The size of the created square.
+        """
+        self.__size = size
