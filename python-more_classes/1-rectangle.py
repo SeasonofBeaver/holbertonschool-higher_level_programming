657e6e1cf51418361618e1b354faafbfd9d28437
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-09-17 11:53:23 +0200

    changed some init stuff

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -18,13 +18,13 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if width < 0:
             raise ValueError("width must be >= 0")
-        self.__width = width
+        self.width = width
 
         if not isinstance(height, int):
             raise TypeError("height must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
-        self.__height = height
+        self.height = height
     
     @property
     def width(self):
