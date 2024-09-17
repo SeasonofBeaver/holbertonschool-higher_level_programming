8da3dda86dafff6b99857fee654d292033b25349
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-09-17 14:21:06 +0200

    testing why I am not getting all the points

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,37 +1,22 @@
 #!/usr/bin/python3
-"""Defines a Class for a Rectangle"""
+"""Define a class for rectangle"""
 
 
 class Rectangle:
-    """A class that defines a Rectangle with height and width"""
+    """
+    Class that represent a square.
+    """
 
     def __init__(self, width=0, height=0):
-        """
-        Initializes a new Rectangle instance.
-
-        Args:
-            width (int): The width of the Rectangle.
-            height (int): The height of the Rectangle.
-        """
         self.width = width
         self.height = height
-    
+
     @property
     def width(self):
-        """width of the Rectangle"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """looks if value is a positive integer otherwise raises error
-
-        Args:
-            value (int): width of the Rectangle given to width.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than 0.
-        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -40,20 +25,10 @@ class Rectangle:
 
     @property
     def height(self):
-        """height of the Rectangle"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """looks if value is a positive integer otherwise raises error
-
-        Args:
-            value (int): height of the Rectangle given to height.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than 0.
-        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
