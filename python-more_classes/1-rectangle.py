48ac7eccf71cbb58dd26314239a99cd5340e9c43
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-09-17 11:59:21 +0200

    removed error codes from init

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,7 +5,6 @@
 class Rectangle:
     """A class that defines a Rectangle with height and width"""
 
-
     def __init__(self, width=0, height=0):
         """
         Initializes a new Rectangle instance.
@@ -14,16 +13,7 @@ class Rectangle:
             width (int): The width of the Rectangle.
             height (int): The height of the Rectangle.
         """
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
         self.width = width
-
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
         self.height = height
     
     @property
