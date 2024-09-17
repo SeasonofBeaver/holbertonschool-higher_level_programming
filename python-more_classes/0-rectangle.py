4ec6a0f80adc8e2f1018fb69a7a7dd21c448fc8f
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-09-17 11:24:27 +0200

    rectangle created in empty class

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a Class for a Rectangle"""
+
+
+class Rectangle:
+    """
+    An empty class to define a Rectangle.
+    For now just a placeholder here for future use.
+    """
+    pass
