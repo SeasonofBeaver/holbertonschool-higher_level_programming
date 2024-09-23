6644b53bdc301d98952834684aa4e1e744d1771d
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-09-23 10:45:17 +0200

    sees if object is an instance in the class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""defines a function that looks if the object is an instance of the class"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Returns True if the object is exactly an instance of the specified class;
+    otherwise False.
+    
+    Args:
+        obj: The object to check.
+        a_class: The class to check against.
+    
+    Returns:
+        bool: True if obj is exactly an instance of a_class, False otherwise.
+    """
+    return type(obj) is a_class
