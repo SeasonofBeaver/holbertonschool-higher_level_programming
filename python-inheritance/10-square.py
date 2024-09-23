cfb44c127222d379aeb123046ac8b48d11dbfcbe
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-09-23 11:50:02 +0200

    created a square that has an area

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines a class Square that inherits from Rectangle."""
+Rectangle = __import__('9-rectangle.py').Rectangle
+
+
+class Square:
+    """Represents a square."""
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Returns the area of the square."""
+        return self.__size ** 2
