a347f81dc2c68f75ec24dc4f91ed0a2ff8ffb126
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-09-23 10:39:38 +0200

    removed init

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,10 +5,6 @@
 class MyList(list):
     """Inherits from the list."""
 
-    def __init__(self):
-        """initializes the object"""
-        super().__init__()
-
     def print_sorted(self):
         """Prints the list, sorted in ascending order."""
         print(sorted(self))
