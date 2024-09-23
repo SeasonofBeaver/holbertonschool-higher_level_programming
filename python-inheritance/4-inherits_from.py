edd4e78939dee73f94a39467a5a97c8cccdafa67
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-09-23 10:54:47 +0200

    sees if object inherited the class, but is not from a class

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+defines a function that looks if the object is an instance of the class
+directly or indirectly
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if the object is an instance of, or if the object is an instance of a class
+    that inherited from, the specified class (directly or indirectly); otherwise False.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to check against.
+
+    Returns:
+        bool: True if obj is an instance or inherited instance of a_class, False otherwise.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
