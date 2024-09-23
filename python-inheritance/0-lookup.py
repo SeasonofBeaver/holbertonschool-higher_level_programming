35b3b35e166dcd41ad23487b9a2e217500dcc642
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-09-23 10:17:07 +0200

    a lookup function for an object

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""defines an object lookup function that returns a list"""
+
+
+def lookup(obj):
+    """returns the list of available attributes and methods of an object"""
+    returns(dir(obj))
