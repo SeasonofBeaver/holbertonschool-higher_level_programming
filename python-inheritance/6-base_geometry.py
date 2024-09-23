6e294a65db4df6ebe5bec025809e4427d6f147fc
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-09-23 11:15:23 +0200

    raises exception as area not implemented yet

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""defines a Base Geometry class"""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    def area(self):
+        """not yet implemented"""
+        raise Exception("area() is not implemented")
