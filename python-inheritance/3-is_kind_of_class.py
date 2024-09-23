6dcbe65bd3fec8a065dfb62f74f53872b08c6d72
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-09-23 10:50:57 +0200

    pep8

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -6,11 +6,11 @@ def is_kind_of_class(obj, a_class):
     """
     Returns True if the object is an instance of, or if the object is an instance of a class
     that inherited from, the specified class; otherwise False.
-    
+
     Args:
         obj: The object to check.
         a_class: The class to check against.
-    
+
     Returns:
         bool: True if obj is an instance or inherited instance of a_class, False otherwise.
     """
