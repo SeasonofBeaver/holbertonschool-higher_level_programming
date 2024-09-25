2f62367565d21db867b7633c4e72306fd54ef0fd
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-09-25 09:40:22 +0200

    iteration through a user input iterable

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines a counted Iterator."""
+from abc import ABC, abstractmethod
+
+
+class CountedIterator(ABC):
+    def __init__(self, some_iterable):
+        """
+        Initializes the class with an iterator from the provided iterable.
+        Also initializes a counter to keep track of how many items are iterated.
+
+        Args:
+            iterable (iterable): Any iterable object like a list, tuple, or string.
+        """
+        self.iterator = iter(some_iterable)
+        self.counter = 0
+
+    def get_count(self):
+        """Returns the aleady counted elements through the iteration"""
+        return self.counter
+
+    def __next__(self):
+        """with every iteration of iter the counter will be updated"""
+        try:
+            item = next(iteration)
+            self.counter += 1
+            return item
+        except StopIteration:
+            raise StopIteration
