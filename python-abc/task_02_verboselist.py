7546928134e2f0c6cd03a032daaef44dd98fcfbf
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-09-25 09:42:12 +0200

    pep8 happy

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -11,17 +11,20 @@ class VerboseList(list):
         print("Added [{}] to the list.".format(item))
 
     def extend(self, iterable):
-        """Extends the list by appending all the items from the iterable and prints a notification."""
+        """Extends the list by appending all the items
+        from the iterable and prints a notification."""
         super().extend(iterable)
         print("Extended the list with [{}] items.".format(list(iterable)))
 
     def remove(self, item):
-        """Removes the first occurrence of an item from the list and prints a notification."""
+        """Removes the first occurrence of an item
+        from the list and prints a notification."""
         super().remove(item)
         print("Removed [{}] from the list.".format(item))
 
     def pop(self, index=-1):
-        """Removes and returns an item at the given index, printing a notification."""
+        """Removes and returns an item at the given index,
+        printing a notification."""
         item = super().pop(index)
         print("Popped [{}] from the list.".format(item))
         return item
