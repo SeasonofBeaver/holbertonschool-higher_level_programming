30280beab0394a042c0c899f7b0adde201ef9fd3
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-09-25 10:02:48 +0200

    removed the tests again

--- python-abc/task_04_flyingfish.py
+++ python-abc/task_04_flyingfish.py
@@ -43,11 +43,3 @@ class FlyingFish(Fish, Bird):
     def habitat(self):
         """overriding the habitat from fish and bird"""
         print("The flying fish lives both in water and the sky!")
-
-if __name__ == "__main__":
-    flying_fish = FlyingFish()
-    flying_fish.fly()
-    flying_fish.swim()
-    flying_fish.habitat()
-
-    print(FlyingFish.mro())
