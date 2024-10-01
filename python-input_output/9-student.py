f1722127949c30533b9932084a955f89868c8d51
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-10-01 11:17:01 +0200

    a standard student class

---
+++ python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a Class of a Student."""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.firstName = first_name
+        self.lastName = last_name
+        self.age = age
+
+    def to_json(self):
+        """returns the dictionary for JSON serialization of an object"""
+        return self.__dict__
