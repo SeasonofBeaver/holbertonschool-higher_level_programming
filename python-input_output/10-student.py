87ddf5b439eab2b8db893ec1ee53077f9e0eef21
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-10-01 11:26:21 +0200

    returns either the full dict or attrs if not none

---
+++ python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines a Class of a Student."""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+            dict: A dictionary containing the instance's attributes.
+        """
+        if attrs is None:
+            return self.__dict__
+        return {k: v for k, v in self.__dict__.items() if k in attrs}
