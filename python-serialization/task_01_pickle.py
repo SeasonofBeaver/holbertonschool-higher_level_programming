64d26d02a83d515ce227adb3b13e62440f673440
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-10-03 10:37:10 +0200

    pickle

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Defines a function in which you create an object."""
+import pickle
+
+
+class CustomObject():
+    """Defines an Object with some attributes."""
+    def __init__(self, name, age, is_student):
+        """gets a name an age and asks if its a student."""
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """Displays the object's attributes."""
+        print("Name: {}".format(self.name))
+        print("Age: {}".format(self.age))
+        print("Is Student: {}".format(self.is_student))
+
+    def serialize(self, filename):
+        """Serializes the CustomObject and saves it to the filename."""
+        try:
+            with open(filename, 'wb') as f:
+                pickle.dump(self, f)
+        except (FileNotFoundError):
+            return None
+
+
+    @classmethod
+    def deserialize(cls, filename):
+        """Deserialize the CustomObject and loads from file."""
+        try:
+            with open(filename, 'rb') as f:
+                obj = pickle.load(f)
+                return obj
+        except (FileNotFoundError, pickle.UnpicklingError):
+            return None
