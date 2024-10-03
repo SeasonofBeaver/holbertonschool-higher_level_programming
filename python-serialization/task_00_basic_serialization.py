08a0711d91927dbc31e837f1df41edcf2eb8f24b
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-10-03 10:13:21 +0200

    pep8 and doc changes

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -4,9 +4,12 @@ import json
 
 
 def serialize_and_save_to_file(data, filename):
+    """Opens a file and and dumps data into it."""
     with open(filename, 'w') as f:
         json.dump(data, f)
 
+
 def load_and_deserialize(filename):
+    """Reads from a file."""
     with open(filename) as f:
         json.load(f)
