41308685648dbf1e47c2faece307289437c061cc
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-10-10 10:28:42 +0200

    http now working

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,41 +1,38 @@
 #!/usr/bin/python3
-import http.server
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
-from http import HTTPStatus
 
 
-class HTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+class HTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/":
-            self.send_response(HTTPStatus.OK)
+            self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
 
         elif self.path == "/data":
-            self.send_response(HTTPStatus.OK)
+            self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
-
-            data = {
-                "name": "John",
-                "age": 30,
-                "city": "New York"
-            }
-
+            data = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(json.dumps(data).encode("utf-8"))
 
         elif self.path == "/status":
-            self.send_response(HTTPStatus.OK)
+            self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"OK")
 
         else:
-            self.send_error(HTTPStatus.NOT_FOUND, "Endpoint Not Found")
+            self.send_error(404, "Endpoint Not Found")
             self.end_headers()
 
-def run(server_class=http.server.HTTPServer, handler_class=HTTPRequestHandler):
-    server_address = ("", 8000)
+def run(server_class=HTTPServer, handler_class=HTTPRequestHandler, port=8000):
+    server_address = ('', port)
     httpd = server_class(server_address, handler_class)
+    print("Starting server on port {}...".format(port))
     httpd.serve_forever()
+
+if __name__ == '__main__':
+    run()
