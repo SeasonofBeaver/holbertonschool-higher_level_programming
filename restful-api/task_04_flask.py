faf0da1e36afe2aadc860e47438a0292414df414
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-10-14 10:28:27 +0200

    corrected wrong tabulation

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -46,9 +46,8 @@ def add_user():
     if not username:
         return jsonify({"error": "Username is required"}), 400
     else:
-    users[username] = data
-
-    return jsonify({"message": "User added", "user": data}), 201
+        users[username] = data
+        return jsonify({"message": "User added", "user": data}), 201
 
 
 if __name__ == "__main__":
