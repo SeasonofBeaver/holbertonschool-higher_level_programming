8bf830c4ac2e1a9b8081ad0b7644d9dbc46e8a2a
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-10-30 10:33:30 +0100

    forgot an s at state

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- Show all cities with its id name and the state name ordered by their ID
 SELECT cities.id, cities.name, states.name FROM cities
-JOIN states ON cities.state_id = state.id
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id;
