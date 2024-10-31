512d3ea29ebd798895a3d3aa36ac48b3d77dd39f
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-10-31 14:15:46 +0100

    changing the SQL command and the print

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -15,10 +15,11 @@ if __name__ == "__main__":
         port=3306
     )
     cursor = db.cursor()
-    cursor.execute("SELECT cities.name FROM cities WHERE cities.state = %s \
+    cursor.execute("SELECT cities.name FROM cities \
+                    JOIN states ON cities.state_id = states.id \
+                    WHERE states.name = %s \
                     ORDER BY cities.id ASC", (sys.argv[4],))
     cities = cursor.fetchall()
-    for city in cities:
-        print(city)
+    print(", ".join([city[0] for city in cities]))
     cursor.close()
     db.close()
