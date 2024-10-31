cb1087f04892dee764c3250988e519bed6f96b46
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-10-31 15:37:20 +0100

    print the first state in the database

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""lists all state objects from the database."""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    engine = create_engine(
+        f'mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/'
+        f'{database_name}'
+        )
+    Session = sessionmaker(bind=engine)
+
+    first_state = Session.query(State).order_by(State.id).first()
+
+    if first_state
+        print(f"{first_state.id}: {first_state.name}")
+    else:
+        print("Nothing")
+
+    Session.close()
