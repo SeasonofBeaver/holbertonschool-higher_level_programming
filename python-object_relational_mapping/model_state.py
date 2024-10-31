f46ce8a75228c845c726f5c006b5ad67b16c65ff
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-10-31 14:45:06 +0100

    changing model state again

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Creates a model of a state."""
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-import sys
 
 Base = declarative_base()
 
@@ -12,11 +11,3 @@ class State(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1],
-                                   sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
