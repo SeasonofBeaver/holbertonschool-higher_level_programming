68f4dbf95c35792936f02916db5669a0e6d4d82b
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-10-31 15:39:58 +0100

    forgot a :

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -18,13 +18,11 @@ if __name__ == "__main__":
         )
     Session = sessionmaker(bind=engine)
 
-    session = Session()
+    first_state = Session.query(State).order_by(State.id).first()
 
-    first_state = session.query(State).order_by(State.id).first()
-
-    if first_state
+    if first_state:
         print(f"{first_state.id}: {first_state.name}")
     else:
         print("Nothing")
 
-    session.close()
+    Session.close()
