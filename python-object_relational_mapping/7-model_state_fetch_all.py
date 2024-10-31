bfd7fc9de6d82c330e384bcb8d1c93c7ff9bbd18
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-10-31 15:02:33 +0100

    changed argv to sys argv

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,9 +8,9 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    mysql_username = argv[1]
-    mysql_password = argv[2]
-    database_name = argv[3]
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
     engine = create_engine(
         f'mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/'
