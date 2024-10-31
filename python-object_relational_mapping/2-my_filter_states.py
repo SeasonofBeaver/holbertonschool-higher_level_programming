6ed40079d419a9e9ff00aef0bdb84d15a9431c6e
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-10-31 10:25:32 +0100

    lists all states with a specific name

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""lists all states that have a specific name"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = '{}' \
+                    ORDER BY id ASC".format(sys.argv[4]))
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    db.close()
